2ea06355409aa1b75c70c4660b4fd81db1141622
Arturs <xyzdelete@protonmail.com>
2022-10-31 19:18:53 +0200

    Create User Defined Type
    It will define Car type and CarSeat type

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,7 +1,21 @@
 #pragma once
 
 #include <JuceHeader.h>
+struct Car
+{
+    struct CarSeat
+    {
+        bool seatIsLeather;
+    };
+    int numberOfWheels;
+    bool convertible;
+    CarSeat driverSeat;
+    CarSeat navigatorsSeat;
 
+    bool switchSeats(CarSeat oldSeat, CarSeat newSeat);
+
+    void accelerate(float howFarToPushThePedal);
+};
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
